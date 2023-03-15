941f79baf9f0a560fb2954ce3768bb2dfedd3019
Mary Cods <maryam.selassie@gmail.com>
2023-03-15 02:32:54 -0700

    CSS Update

---
+++ script.js
@@ -0,0 +1,6 @@
+var weatherURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+var APIKEY = '8b898c3025d1ef3a71e8de4af256aea3';
+var lon;
+var lat;
+var searchLocation;
+var eventResult;
