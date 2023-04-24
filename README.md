37c679c2ee97dd538ed718823da3e9c796feb40f
Mary Cods <maryam.selassie@gmail.com>
2023-04-24 14:32:46 -0700

    exit

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Weather-Dashii
+# Description
+This application is a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+# Installation
+N/A
+
+# Credit
+N/A
+
+## Visual 
+![forcast](./images/weather-hp.jpg)
+
+
+## Technologies
+HTML, Tailwind.css, Javascript, jQuery
+
+
+## Links
+
+[GitHub Repo]
+
+
+
+[Deployed Link]
+
+
+# License
+   
+[![License](https://img.shields.io/badge/license-MIT-lightgrey.svg)](https://opensource.org/licenses/MIT)
+
+# Features
+Generates weather based on the search bar input
+
+# Contributions
+N/A
--- script.js
+++ script.js
@@ -1,46 +1,66 @@
 var weatherURL = `https://api.openweathermap.org/data/2.5/forecast?`;
+
 var APIKEY = '8b898c3025d1ef3a71e8de4af256aea3';
-let  city = inputBar.value;
-var geocoderURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city},CA,USA&appid=${APIKEY}`;
+var city = "";
+var geocoderURL = "";
+var currentUrl = "";
 
 
-// var lon;
-// var lat;
+//  var lon;
+//  var lat;
 var searchLocation;
 var eventResult;
 
 var weathCards = $('.weather-posts');
 var weathEL = $('.weath-place');
 var currentDeg = $('.current-temp');
+var currWeather = $('.current-weather');
 var highTemp = $('.high');
+var hTemp = $('.high-temp');
 var lowTemp = $('.low');
+var lTemp = $('.low-temp');
 var weatherDes = $('.description');
+var weathDes = $('#description');
+
 var weathDate = $('.date');
 var weathButton = $('.button');
-var inputBar = $("#user-input");
+var inputBar = $('#user-input');
+var humW = $('.humid');
+var humidityW = $('#humidity');
+//  var degreeP =  $('#icon');
 
-// function parsedLocation(result) {
-//   lon = result.center[0];
-//   lat = result.center[1];
-//   weathEL.text("Weather in" + result.place_name);
-//   // getWeather(lon, lat);
-// }
-
-
-
-function getWeather() {
-  // fetch(geocoderURL + '&limit=5 + &units=imperial&appid=')
-  // .then(function(response){
-  //   return response.json();
-  // })
-  // .then(function(data){
-  //   console.log(data)
-  // })
-
-  fetch(geocoderURL + city)
+function getgeoCoderdata() {
+  return fetch(geocoderURL)
     .then(function (response) {
       return response.json();
     })
+
+}
+
+
+function getWeather() {
+
+  getgeoCoderdata()
+    .then(function (data) {
+      const currURL = currentUrl + '&lat=' + data[0].lat + '&lon=' + data[0].lon + "&units=imperial&appid=" + APIKEY;
+      
+      fetch(currURL)
+        .then(function (response) {
+          return response.json();
+        }).then(function (currentweatherData) {
+          console.log(currentweatherData);
+          currWeather.text(currentweatherData.main.temp);
+
+          hTemp.text(currentweatherData.main.temp_max);
+
+          lTemp.text(currentweatherData.main.temp_min);
+          humidityW.text(currentweatherData.main.humidity);
+
+          weathDes.text(currentweatherData.weather[0].description);
+
+        })
+    })
+  getgeoCoderdata()
     .then(function (data) {
       fetch(weatherURL + '&lat=' + data[0].lat + '&lon=' + data[0].lon + "&units=imperial&appid=" + APIKEY)
         .then(function (response) {
@@ -48,22 +68,25 @@ function getWeather() {
         })
         .then(function (data) {
           console.log(data);
+          var j = 0;
+          for (var i = 3; i < data.list.length; i = i + 8) {
 
-          for (var i = 0; i < weathCards.length; i++) {
-           
-           
-            currentDeg[i].textContent = data.list[i].main.temp;
-           
-            highTemp[i].textContent = data.list[i].main.temp_max;
-            
-            lowTemp[i].textContent = data.list[i].main.temp_min;
-            
-            weatherDes[i].textContent = data.list[i].weather[0].description;
-            //  weathDate[i].textContent = dayjs.unix(data.list.dt_txt[i]).format('M/D/YY');
-            console.log(i);
-            
+            currentDeg[j].textContent = data.list[i].main.temp;
+
+            highTemp[j].textContent = data.list[i].main.temp_max;
+
+            lowTemp[j].textContent = data.list[i].main.temp_min;
+            lowTemp[j].textContent = data.list[i].main.temp_min;
+            console.log(humW,j);
+            humW[j].textContent = data.list[i].main.humidity;
+
+            weatherDes[j].textContent = data.list[i].weather[0].description;
+            weathDate[j].textContent = moment.unix(data.list[i].dt).format('M/D/YY');
+            console.log(j);
+            j++;
+
           }
-         
+
         }
         )
       console.log(data);
@@ -84,16 +107,21 @@ function getWeather() {
 // };
 
 function loadWeather() {
-  inputBar = localStorage.getItem('#user-input');
-  lon = localStorage.getItem('lon');
-  lat = localStorage.getItem('lat');
-
-  weathEL.text("Weather for " + inputBar);
-  getWeather(lon, lat);
 
   weathButton.on("click", function () {
-    inputBar = input.value;
-    console.log(searchLocation);
+    city = inputBar.val();
+    var currentStorage = [];
+    currentStorage.push(localStorage.getItem('input-bar'));
+    currentStorage.push(city);
+    document.getElementById('user-input').value = currentStorage[currentStorage.length-1]
+    
+    localStorage.setItem("input-bar", currentStorage, JSON.stringify());
+    currentUrl = `https://api.openweathermap.org/data/2.5/weather?`
+    geocoderURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city},US&appid=${APIKEY}`
+    weathEL.text("Weather for " + city);
+    getWeather();
+
+
   })
 
 };
